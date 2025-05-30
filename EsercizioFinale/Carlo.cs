f7318f0db8788dccfb04bdbd3e5431262de42c59
carlocond <carlocondello05@gmail.com>
2025-05-30 16:25:42 +0200

    Update Carlo.cs
    Prima scrittura dei pattern Observer e Decorator

--- EsercizioFinale/Carlo.cs
+++ EsercizioFinale/Carlo.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//OBSERVER PATTERN - CARLO
+public interface IObserver
+{
+    void Notifica(string messaggio);
+}
+public class Ordine : IObserver
+{
+    public string Nome { get; set; }
+    public string Descrizione { get; set; }
+
+    public Ordine(string nome, string descrizione)
+    {
+        Nome = nome;
+        Descrizione = descrizione;
+    }
+
+    public void Notifica(string messaggio)
+    {
+        System.Console.WriteLine($"Ordine {Nome} notificato: {messaggio}");
+    }
+}
+
+
+//DECORATOR PATTERN - CARLO
+public interface INImballaggio
+{
+    string Descrizione();
+}
+public class ProdottoBase : INImballaggio
+{
+    private string _nome;
+    public ProdottoBase(string nome)
+    {
+        _nome = nome;
+    }
+    public string Descrizione()
+    {
+        return _nome;
+    }
+}
+public abstract class DecoratorImballaggio : INImballaggio
+{
+    protected INImballaggio _prodotto;
+
+    public DecoratorImballaggio(INImballaggio prodotto)
+    {
+        _prodotto = prodotto;
+    }
+
+    public virtual string Descrizione()
+    {
+        return _prodotto.Descrizione();
+    }
+}
+
+public class Inscatolato : DecoratorImballaggio
+{
+    public Inscatolato(INImballaggio prodotto) : base(prodotto) { }
+
+    public override string Descrizione()
+    {
+        return _prodotto.Descrizione() + " (Inscatolato)";
+    }
+}
+
+public class Regalo : DecoratorImballaggio
+{
+    public Regalo(INImballaggio prodotto) : base(prodotto) { }
+
+    public override string Descrizione()
+    {
+        return _prodotto.Descrizione() + " (Regalo)";
+    }
+}
