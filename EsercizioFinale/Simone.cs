c1d2235d0e6c55f245f9b45553e270f79bc32bfd
chimchar101 <simoneaddesso25@gmail.com>
2025-05-30 16:30:33 +0200

    Update Simone.cs

--- EsercizioFinale/Simone.cs
+++ EsercizioFinale/Simone.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface IProdotto
+{
+    string Descrizione();
+}
+
+public class ProdottoIgiene : IProdotto
+{
+    public string Descrizione()
+    {
+        return "Sgrassatore Multiuso";
+    }
+
+    public double Prezzo()
+    {
+        return 3.75;
+    }
+}
+
+public class ProdottoAlimentare : IProdotto
+{
+    public string Descrizione()
+    {
+        return "Casco di Banane";
+    }
+
+    public double Prezzo()
+    {
+        return 1.49;
+    }
+}
+
+public class ProdottoElettronica : IProdotto
+{
+    public string Descrizione()
+    {
+        return "Powerbank Cellulare";
+    }
+
+    public double Prezzo()
+    {
+        return 39.99;
+    }
+}
+
+public static class ProdottoFactory
+{
+    public static IProdotto CreaProdotto(string prodotto)
+    {
+        switch (prodotto)
+        {
+            case "1":
+                return new ProdottoIgiene();
+            case "2":
+                return new ProdottoAlimentare();
+            case "3":
+                return new ProdottoElettronica();
+            default:
+                Console.WriteLine("ERRORE INPUT - Ritorno al menù.");
+                return null;
+        }
+    }
+}
